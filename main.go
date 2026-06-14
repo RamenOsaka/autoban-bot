2ed0ada235bf4ea701c06d42c033e2ceb153e642
RamenOsaka <118491791+RamenOsaka@users.noreply.github.com>
2026-06-14 00:36:23 -0400

    slash commands working, two commands added, /ping to ping the bot and /addrole to add a role ID to the list of banned roles

--- main.go
+++ main.go
@@ -12,10 +12,12 @@ import (
 )
 
 var bannedRoles = []string{"1514834166892597420"}
+var serverID = "1260943648695255140"
+var commandList = []discordgo.ApplicationCommand{}
 
 func main() {
 	// Setting up discord token
-	discordToken := loadToken()
+	discordToken, appID := loadEnv()
 	dg, err := discordgo.New("Bot " + discordToken)
 	if err != nil {
 		log.Println("Error creating Discord session: ", err)
@@ -26,6 +28,7 @@ func main() {
 	dg.AddHandler(ready)
 	dg.AddHandler(handlerRoleUpdate)
 	dg.AddHandler(handlerRoleOnJoin)
+	dg.AddHandler(handlerInteraction)
 
 	// Setting up discord Intents
 	dg.Identify.Intents = discordgo.IntentGuildMembers
@@ -33,6 +36,33 @@ func main() {
 	err = dg.Open()
 	if err != nil {
 		log.Println("Error opening Discord session: ", err)
+	}
+
+	addRole := &discordgo.ApplicationCommand{
+		Name:        "addrole",
+		Description: "Adds a role to the autoban role list",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionRole,
+				Name:        "role",
+				Description: "role to be added",
+				Required:    true,
+			},
+		},
+	}
+	commandList = append(commandList, *addRole)
+
+	pingCommand := &discordgo.ApplicationCommand{
+		Name:        "ping",
+		Description: "Hello there",
+	}
+	commandList = append(commandList, *pingCommand)
+
+	for i := 0; i < len(commandList); i++ {
+		_, err = dg.ApplicationCommandCreate(appID, serverID, &commandList[i])
+		if err != nil {
+			log.Println("Error creating a new command : ", err)
+		}
 	}
 
 	log.Println("Autoban is now running.  Press CTRL-C to exit.")
@@ -65,7 +95,37 @@ func handlerRoleUpdate(s *discordgo.Session, m *discordgo.GuildMemberUpdate) {
 	banRole(s, m.Member)
 }
 
-func loadToken() string {
+func handlerInteraction(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.Type == discordgo.InteractionApplicationCommand {
+		if i.ApplicationCommandData().Name == "ping" {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: 4,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Pong!",
+				},
+			})
+		} else if i.ApplicationCommandData().Name == "addrole" {
+			if slices.Contains(bannedRoles, i.ApplicationCommandData().Options[0].RoleValue(s, serverID).ID) {
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: 4,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Role **" + i.ApplicationCommandData().Options[0].RoleValue(s, serverID).Name + "** is already in the banned list role.",
+					},
+				})
+			} else {
+				bannedRoles = append(bannedRoles, i.ApplicationCommandData().Options[0].RoleValue(s, serverID).ID)
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: 4,
+					Data: &discordgo.InteractionResponseData{
+						Content: "Role **" + i.ApplicationCommandData().Options[0].RoleValue(s, serverID).Name + "** has been added to the banned list role.",
+					},
+				})
+			}
+		}
+	}
+}
+
+func loadEnv() (string, string) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -75,5 +135,9 @@ func loadToken() string {
 	if !exists {
 		log.Println("DISCORD_TOKEN is not set!")
 	}
-	return token
+	appID, exists := os.LookupEnv("APP_ID")
+	if !exists {
+		log.Println("APP_ID is not set!")
+	}
+	return token, appID
 }
